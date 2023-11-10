18682ae0594a92a00bd53409fa2f8bbe9985fe0d
Diana Rios <dianamarcelacespedesrios85@gmail.com>
2023-11-10 14:59:16 -0500

    actualizacion

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //toggle icon navbar
-
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
+  menuIcon.classList.toggle("bx-x")
+  navbar.classList.toggle("active")
+
+}
+
 
 //scroll sections
 
@@ -37,6 +38,4 @@ window.onscroll = () => {
 
   // remove toggle icon and navbar when click navbar links(scroll)
 
-  menuIcon.classList.remove("bx-x");
-  navbar.classList.remove("active");
-};
+}
