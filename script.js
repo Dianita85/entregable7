9bf82ac5acc9f395d631a14d52e719f58dc52a29
Diana Rios <dianamarcelacespedesrios85@gmail.com>
2023-11-10 15:44:45 -0500

    actuaal

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //toggle icon navbar
 
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 //scroll sections
 
@@ -31,4 +37,6 @@ window.onscroll = () => {
 
   // remove toggle icon and navbar when click navbar links(scroll)
 
-}
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
